aa0ab7a9ccfbd9e4f595ef61c36ea6c32079f522
DoubleSpace <billy.lundh@gmail.com>
2023-05-05 10:55:26 +0200

    Nya endpoints
    /api/v1/events/latest
    /api/v1/events/<string:event_type>/count

--- app.py
+++ app.py
@@ -95,5 +95,48 @@ def create_event():
     response[0].headers.set('Location', f'/api/v1/events/{event.external_id}')
     return response
 
+
+   @app.route('/api/v1/events/<string:event_type>/count', methods=['GET'])
+def count_events_by_type(event_type):
+    events = Event.query.filter_by(type=event_type).all()
+    count = len(events)
+
+    return jsonify({'count': count}), 200
+
+
+
+@app.route('/api/v1/events/latest', methods=['GET'])
+def get_latest_events():
+    events = Event.query.order_by(Event.datetime.desc()).limit(10).all()
+
+    events_list = [{
+        "id": event.external_id,
+        "title": event.title,
+        "summary": event.summary,
+        "url": event.url,
+        "type": event.type,
+        "location": event.location,
+        "datetime": event.datetime
+    } for event in events]
+
+    return jsonify(events_list), 200
+
+
+@app.route('/api/v1/events/latest', methods=['GET'])
+def get_latest_events():
+    events = Event.query.order_by(Event.datetime.desc()).limit(10).all()
+
+    events_list = [{
+        "id": event.external_id,
+        "title": event.title,
+        "summary": event.summary,
+        "url": event.url,
+        "type": event.type,
+        "location": event.location,
+        "datetime": event.datetime
+    } for event in events]
+
+    return jsonify(events_list), 200
+
 if __name__ == '__main__':
     app.run(debug=True)
